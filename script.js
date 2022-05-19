9edc20a744190a515cfe81b60c6c27973150a7aa
Viktar <viktarbrutsis@gmail.com>
2022-05-19 15:20:52 +0300

    add script.js and index.html

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let numberOfFiles = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+   
+}
+
+personalMovieDB.count = numberOfFiles;
+personalMovieDB.movies = {};
+personalMovieDB.actors = {};
+personalMovieDB.genres = [];
+removeEventListener.privat = false;
+
+let lastFim = prompt('Один из последних просмотренных фильмов?', '');
+let critic = prompt('На сколько оцените его?', '');
+
+let movies = {
+
+}
+
+movies.[lastFilm] = critic;
