6cd92918ccb464ffea6536b5ac4562671b07a13a
Viktar <viktarbrutsis@gmail.com>
2022-05-23 15:47:56 +0300

    triangle loop

--- loops.js
+++ loops.js
@@ -1,6 +1,19 @@
-for (let i = 0; i < 3; i++) {
-   console.log(i);
-   for (let j = 0; j < 3; j++) {
-      console.log(j);
-   }
-}
+//for (let i = 0; i < 3; i++) {
+//   console.log(i);
+//   for (let j = 0; j < 3; j++) {
+//      console.log(j);
+//   }
+//}
+
+//*
+//**
+//***
+//****
+//*****
+//******
+
+let str = '';
+for (let i = 1; i <= 6; i++) {
+   str += *;
+   console.log(str);
+} 
