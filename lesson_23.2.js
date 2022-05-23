83cf71ac1f7a0dbc57654c1afd0248453d67415a
Viktar Brutsis <viktarbrutsis@gmail.com>
2022-05-23 20:35:04 +0300

    added third task

--- lesson_23.2.js
+++ lesson_23.2.js
@@ -29,3 +29,14 @@ function secondTask() {
     
     return data;
 }
+
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+    for (let i = 0; i < data.length; i++) {
+        result[i] = data[data.length - 1 - i];
+    }
+
+    return result;
+}
