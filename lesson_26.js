aa0e33471991f590c9dd9c214102e297d2f19a0d
Viktar brutsis <viktarbrutsis@gmail.com>
2022-06-06 22:44:40 +0300

    finished lesson_26

--- lesson_26.js
+++ lesson_26.js
@@ -1,4 +1,44 @@
-function convert(amount) {
-    console.log(28 * amount);
+const usdCurr = 28;
+const eurCurr = 32;
+
+function convert(amount, curr) {
+    console.log(curr * amount);
 }
-convert(500);
+convert(500, usdCurr);
+convert(500, eurCurr);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+    console.log(i);
+    if (i === 3) return i
+    }
+}
+const res = test();
+// console.log(res);
+
+function sayHello(name) {
+    console.log(`Привет ${name}`)
+    
+}
+sayHello('Viktar');
+
+function getMathResult(a, b) {
+    let result = a;
+
+    if ( b <=0 || (typeof(b) == 'string')) {
+        console.log(a);
+    }
+    else if (a === b) {
+        console.log(result);
+    }
+    else {
+        for ( let i = 2; i <= b; i++) {
+            result = result + '---' + a * i; 
+            
+        }
+        console.log(result);
+            
+    }
+    return
+}
+getMathResult(3, 3);
