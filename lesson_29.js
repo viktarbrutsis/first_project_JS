f17a56483f7dcdd264f6d284e1ade62bba600bc9
Viktar <viktarbrutsis@gmail.com>
2022-06-07 11:40:48 +0300

    finished 29

--- lesson_29.js
+++ lesson_29.js
@@ -21,52 +21,52 @@ let personalMovieDB = {
    
 };
 
-function rememberMyFilms() {
-   for (let i = 0; i < 2; i++) {
-      let lastFilm_1 = prompt('Один из последних просмотренных фильмов?', ''),
-          critic_1 = prompt('На сколько оцените его?', '');
+//function rememberMyFilms() {
+//   for (let i = 0; i < 2; i++) {
+//      let lastFilm_1 = prompt('Один из последних просмотренных фильмов?', ''),
+//          critic_1 = prompt('На сколько оцените его?', '');
    
-      if (lastFilm_1 != null && critic_1 != null && lastFilm_1 != '' && critic_1 != '' && lastFilm_1.length < 50 && critic_1.length < 50) {
-         personalMovieDB.movies[lastFilm_1] = critic_1;
-      } else {
-         console.log('error');
-         i--;
-      }  
+//      if (lastFilm_1 != null && critic_1 != null && lastFilm_1 != '' && critic_1 != '' && lastFilm_1.length < 50 && critic_1.length < 50) {
+//         personalMovieDB.movies[lastFilm_1] = critic_1;
+//      } else {
+//         console.log('error');
+//         i--;
+//      }  
    
-   }
-}
+//   }
+//}
 
-rememberMyFilms();
+//rememberMyFilms();
 
-function detectPersonalLevel () {
-   if (personalMovieDB.count < 10) {
-      console.log("Просмотрено слишком мало фильмов!!!");
-   } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-      console.log("Достойный результат!!!");
-   } else if (personalMovieDB.count >= 30) {
-      console.log("Вы киноман!!!");
-   } else {
-      console.log("Прозошла ошибка");
-   }
-}
+//function detectPersonalLevel () {
+//   if (personalMovieDB.count < 10) {
+//      console.log("Просмотрено слишком мало фильмов!!!");
+//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//      console.log("Достойный результат!!!");
+//   } else if (personalMovieDB.count >= 30) {
+//      console.log("Вы киноман!!!");
+//   } else {
+//      console.log("Прозошла ошибка");
+//   }
+//}
 
-detectPersonalLevel();
+//detectPersonalLevel();
 
-function showMyDB(info) {
-   if (info == false) {
+function showMyDB() {
+   if (personalMovieDB.privat == false) {
       console.log(personalMovieDB);
    }
 }
 
-showMyDB(personalMovieDB.privat);
+showMyDB();
 
-function writeYourGenres() {
-   for (let i = 0; i < 3; i++) {
-      personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-   }
-   console.log(personalMovieDB.genres);
-}
+//function writeYourGenres() {
+//   for (let i = 0; i < 3; i++) {
+//      personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+//   }
+//   console.log(personalMovieDB.genres);
+//}
 
-writeYourGenres();
+//writeYourGenres();
 
 
