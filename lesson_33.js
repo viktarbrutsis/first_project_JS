12ca53412ce3077fef9bbc74b0d11adfe45319cf
viktarbrutsis <105799221+viktarbrutsis@users.noreply.github.com>
2022-06-09 08:30:26 +0300

    Update lesson_33.js

--- lesson_33.js
+++ lesson_33.js
@@ -26,3 +26,32 @@ console.log(arr);
 function compareNum (a, b) {
    return a - b;
 }
+
+//const arr = [2, 4, 7, 12, 56, 40];
+
+//arr.forEach(function(item, i, arr) {
+//   console.log(`${i}: ${item} внутри массива ${arr}`);
+//});
+
+//for (let value of arr) {
+//   console.log(value);
+//}
+
+//const str = prompt('', '');
+//const products = str.split(",");
+//console.log(products);
+
+//const names = ['Ivan', 'Daria', 'Olga', 'Vic', 'Natasha'];
+//console.log(names.join('; '));
+
+//const names = ['Ivan', 'Daria', 'Olga', 'Vic', 'Natasha'];
+//const newNames = names.sort();
+//console.log(newNames);
+
+const arr = [34, 943, 6, 2, 4, 7, 12, 56, 40];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum (a, b) {
+   return a - b;
+}
